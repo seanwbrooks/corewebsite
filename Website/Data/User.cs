782670cc68812df781bdaf2a8820182f131d9618
Sean <swbrooks87@gmail.com>
2019-04-21 10:51:56 -0400

    Add db context, user class, and update hello page with form

---
+++ Website/Data/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Website.Data
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required, StringLength(100)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string AddressLine1 { get; set; }
+        [StringLength(50)]
+        public string AddressLine2 { get; set; }
+        [StringLength(50)]
+        public string City { get; set; }
+        [StringLength(50)]
+        public string State { get; set; }
+        [MaxLength(9)]
+        public int ZipCode { get; set; }
+
+    }
+}
--- Website/Startup.cs
+++ Website/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RazorPagesHello.Data;
 
 namespace Website
 {
@@ -21,6 +22,7 @@ namespace Website
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("name"));
             services.AddMvc();
         }
 
