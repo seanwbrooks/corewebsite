57c4c5182fae07d019e2f9c4dfab79f2ab732044
Sean <swbrooks87@gmail.com>
2019-10-31 10:54:54 -0400

    Add marathon plan page and update to strava link and initial intro

---
+++ Website/Pages/MarathonPlan.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages
+{
+    public class MarathonPlanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public object OnSubmit(object formData)
+        {
+            return formData;
+        }
+    }
+}
